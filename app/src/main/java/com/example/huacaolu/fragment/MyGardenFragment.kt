2763ba9150f384c77af48ab51f91ac6d58958b11
HacLau <liushunlin940720@126.com>
2022-06-02 15:08:09 +0800

    收藏的数据进行百度百科页面跳转

--- app/src/main/java/com/example/huacaolu/fragment/MyGardenFragment.kt
+++ app/src/main/java/com/example/huacaolu/fragment/MyGardenFragment.kt
@@ -2,22 +2,30 @@ package com.example.huacaolu.fragment
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ListView
 import androidx.fragment.app.Fragment
 import com.example.huacaolu.R
+import com.example.huacaolu.activity.SearchResultDetails
+import com.example.huacaolu.activity.WebActivity
 import com.example.huacaolu.adapter.MyCollectionAdapter
 import com.example.huacaolu.bean.ExplorePlantBean
+import com.example.huacaolu.bean.SearchImagePlantBean
 import com.example.huacaolu.helper.DataBasePlantHelper
 import com.example.huacaolu.utils.DataBaseUtil
+import com.google.gson.Gson
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class MyGardenFragment : Fragment(){
+class MyGardenFragment : Fragment(), AdapterView.OnItemClickListener {
     private var param1: String? = null
     private var param2: String? = null
     private lateinit var mCollectionList: ArrayList<ExplorePlantBean.Result>
@@ -55,7 +63,7 @@ class MyGardenFragment : Fragment(){
 
     private fun initView(view: View) {
         mCollection = view.findViewById<ListView>(R.id.lv_collection)
-
+        mCollection.onItemClickListener = this
     }
 
 
@@ -97,6 +105,15 @@ class MyGardenFragment : Fragment(){
             }while (cursor.moveToNext())
         }
         return resultList
+    }
+
+    override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        val url = mCollectionList[p2].url
+        if (!TextUtils.isEmpty(url)) {
+            val intent = Intent(requireContext(), WebActivity::class.java)
+            intent.putExtra("url",url)
+            startActivity(intent)
+        }
     }
 
 }
